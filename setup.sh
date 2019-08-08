8e13de2f4aa05de6604107c2824bdd6bb4ea1e17
Ian Finch <ianfinch@users.noreply.github.com>
2019-08-08 10:56:23 +0000

    Added tinygo

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPTNAME=$(basename $0)
+SCRIPTNAME=$(basename "$0")
 runningLocally=false
 
 __help() {
@@ -77,12 +77,12 @@ __identifyWindowsUser() {
 
 __initCounter() {
     n=1
-    max=$(grep '^__step ' $0 | wc -l)
+    max=$(grep '^__step ' "$0" | wc -l)
 }
 
 
 __initVariables() {
-    DOCKER_SCRIPTS="`dirname $0`"
+    DOCKER_SCRIPTS=$(dirname "$0")
     STAGE_BIN="/tmp/stage-bin"
     LOCAL_BIN="${HOME}/.bin"
     BIN="/usr/local/bin"
@@ -90,6 +90,7 @@ __initVariables() {
     RESOURCES="${DOCKER_SCRIPTS}/resources"
     CACHE="${DOCKER_SCRIPTS}/cache"
     VM_HOME="/home/docker"
+    GENERIC_COMMAND="${DOCKER_SCRIPTS}/generic-command"
     __initCounter
 }
 
@@ -107,43 +108,45 @@ __setupUtilities() {
 
     # Anything from our base image
     echo "tree,drill," | while read -d ',' cmd ; do
-        ${DOCKER_SCRIPTS}/generic-command $cmd guzo/base > ${STAGE_BIN}/$cmd
+        "$GENERIC_COMMAND" $cmd guzo/base > ${STAGE_BIN}/$cmd
     done;
 
     # Commands which only have an x86 version
     echo "vim,perl,gcloud,vue,tig,zip," | while read -d ',' cmd ; do
-        ${DOCKER_SCRIPTS}/generic-command $cmd > ${STAGE_BIN}/$cmd
+        "$GENERIC_COMMAND" $cmd > ${STAGE_BIN}/$cmd
     done;
 
     # Commands which also have an ARM version
     echo "npm," | while read -d ',' cmd ; do
-        ${DOCKER_SCRIPTS}/generic-command $cmd x86_64=guzo/$cmd,armv7l=guzo/$cmd:rpi > ${STAGE_BIN}/$cmd
+        "$GENERIC_COMMAND" $cmd x86_64=guzo/$cmd,armv7l=guzo/$cmd:rpi > ${STAGE_BIN}/$cmd
     done;
 
     # Special cases
-    ${DOCKER_SCRIPTS}/generic-command node x86_64=guzo/npm,armv7l=guzo/npm:rpi -p 3000:3000 > ${STAGE_BIN}/node
-    ${DOCKER_SCRIPTS}/generic-command go golang:alpine > ${STAGE_BIN}/go
-    ${DOCKER_SCRIPTS}/generic-command ngrok guzo/ngrok -p 4040:4040 > ${STAGE_BIN}/ngrok
-    ${DOCKER_SCRIPTS}/generic-command lein guzo/leinjs -v /home/docker/.lein:/home/ian/.lein -v /run/m2:/home/ian/.m2 -p 3000:3000 > ${STAGE_BIN}/lein
-    ${DOCKER_SCRIPTS}/generic-command java java:alpine -p 3000:3000 > ${STAGE_BIN}/java
-    ${DOCKER_SCRIPTS}/generic-command mvn maven > ${STAGE_BIN}/mvn
-    ${DOCKER_SCRIPTS}/generic-command hugo x86_64=guzo/hugo,armv7l=guzo/hugo:rpi -p 8888:8888 > ${STAGE_BIN}/hugo
-    ${DOCKER_SCRIPTS}/generic-command npm guzo/npm -p 3000:3000 -v /run/node_dependencies:/usr/src/dependencies > ${STAGE_BIN}/npm
-    ${DOCKER_SCRIPTS}/generic-command task guzo/task -v ${VM_HOME}/task-data:/usr/src/data > ${STAGE_BIN}/task
-    ${DOCKER_SCRIPTS}/generic-command http guzo/httpie > ${STAGE_BIN}/http
-    ${DOCKER_SCRIPTS}/generic-command gpg guzo/gpg -v /run/gnupg:/home/ian/.gnupg -v /dev/urandom:/dev/random > ${STAGE_BIN}/gpg
+    "$GENERIC_COMMAND" node x86_64=guzo/npm,armv7l=guzo/npm:rpi -p 3000:3000 > ${STAGE_BIN}/node
+    "$GENERIC_COMMAND" go golang:alpine -e '"GOOS=$GOOS"' -e '"GOARCH=$GOARCH"' -p 3000:3000 > ${STAGE_BIN}/go
+    "$GENERIC_COMMAND" tinygo tinygo/tinygo > ${STAGE_BIN}/tinygo
+    "$GENERIC_COMMAND" ngrok guzo/ngrok -p 4040:4040 > ${STAGE_BIN}/ngrok
+    "$GENERIC_COMMAND" lein guzo/leinjs -v /home/docker/.lein:/home/ian/.lein -v /run/m2:/home/ian/.m2 -p 3000:3000 > ${STAGE_BIN}/lein
+    "$GENERIC_COMMAND" java java:alpine -p 3000:3000 > ${STAGE_BIN}/java
+    "$GENERIC_COMMAND" mvn maven > ${STAGE_BIN}/mvn
+    "$GENERIC_COMMAND" hugo x86_64=guzo/hugo,armv7l=guzo/hugo:rpi -p 8888:8888 > ${STAGE_BIN}/hugo
+    "$GENERIC_COMMAND" npm guzo/npm -p 3000:3000 -v /run/node_dependencies:/usr/src/dependencies > ${STAGE_BIN}/npm
+    "$GENERIC_COMMAND" task guzo/task -v ${VM_HOME}/task-data:/usr/src/data > ${STAGE_BIN}/task
+    "$GENERIC_COMMAND" http guzo/httpie > ${STAGE_BIN}/http
+    "$GENERIC_COMMAND" gpg guzo/gpg -v /run/gnupg:/home/ian/.gnupg -v /dev/urandom:/dev/random > ${STAGE_BIN}/gpg
 
     # Custom scripts
-    cp ${DOCKER_SCRIPTS}/dot-server ${STAGE_BIN}/dot
-    cp ${DOCKER_SCRIPTS}/swagger ${STAGE_BIN}/swagger
+    cp "${DOCKER_SCRIPTS}/dot-server" ${STAGE_BIN}/dot
+    cp "${DOCKER_SCRIPTS}/swagger" ${STAGE_BIN}/swagger
 
     # Temporary commands
     echo "docker run -ti -v ${WINDOWS_HOME}/Downloads/iplayer:/opt/iplayer guzo/iplayer get_iplayer --output /opt/iplayer/output --profile-dir /opt/iplayer/profile" '$*' > ${STAGE_BIN}/iplayer
 
     # Some docker utilities
-    cp ${DOCKER_SCRIPTS}/docker-clean ${STAGE_BIN}/docker-clean
-    cp ${DOCKER_SCRIPTS}/docker-kill ${STAGE_BIN}/docker-kill
-    cp ${DOCKER_SCRIPTS}/docker-repo ${STAGE_BIN}/docker-repo
+    cp "${DOCKER_SCRIPTS}/docker-setup" ${STAGE_BIN}/docker-setup
+    cp "${DOCKER_SCRIPTS}/docker-clean" ${STAGE_BIN}/docker-clean
+    cp "${DOCKER_SCRIPTS}/docker-kill" ${STAGE_BIN}/docker-kill
+    cp "${DOCKER_SCRIPTS}/docker-repo" ${STAGE_BIN}/docker-repo
 
     # Move commands from our staging area
     chmod +x ${STAGE_BIN}/*
@@ -187,26 +190,30 @@ __setupPath() {
 }
 
 
-__setupGlobalPrompt() {
-    cp ${RESOURCES}/set-prompt.sh /etc/profile.d/set-prompt.sh
+__setupGlobalProfileFiles() {
+    cp "${RESOURCES}/set-prompt.sh" /etc/profile.d/set-prompt.sh
     chmod +x /etc/profile.d/set-prompt.sh
+    cp "${RESOURCES}/bind-clear-screen.sh" /etc/profile.d/bind-clear-screen.sh
+    chmod +x /etc/profile.d/bind-clear-screen.sh
 }
 
 
 
-__setupLocalPrompt() {
-    cp ${RESOURCES}/set-prompt.sh ${LOCAL_BIN}/.set-prompt.sh
+__setupLocalProfileFiles() {
+    cp "${RESOURCES}/set-prompt.sh" ${LOCAL_BIN}/.set-prompt.sh
+    cp "${RESOURCES}/set-prompt.sh" ${LOCAL_BIN}/.bind-clear-screen.sh
     if [[ ! -e $HOME/.profile || $(grep set-prompt.sh $HOME/.profile) == "" ]] ; then
         echo "source ${LOCAL_BIN}/.set-prompt.sh" >> $HOME/.profile
+        echo "source ${LOCAL_BIN}/.bind-clear-screen.sh" >> $HOME/.profile
     fi
 }
 
 
-__setupPrompt() {
+__setupProfileFiles() {
     if [[ $runningLocally == true ]] ; then
-        __setupLocalPrompt
+        __setupLocalProfileFiles
     else
-        __setupGlobalPrompt
+        __setupGlobalProfileFiles
     fi
 }
 
@@ -221,8 +228,8 @@ __installBash() {
 
 
 __createCacheDir () {
-    if [[ ! -e ${CACHE} ]] ; then
-        mkdir ${CACHE}
+    if [[ ! -e "${CACHE}" ]] ; then
+        mkdir "${CACHE}"
     fi
 }
 
@@ -230,15 +237,15 @@ __createCacheDir () {
 __installDockerCompose()  {
     __createCacheDir
 
-    if [[ ! -e ${CACHE}/docker-compose ]] ; then
-        curl -Ls "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o ${CACHE}/docker-compose
+    if [[ ! -e "${CACHE}/docker-compose" ]] ; then
+        curl -Ls "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o "${CACHE}/docker-compose"
     fi
 
     if [[ $runningLocally == true ]] ; then
-        cp ${CACHE}/docker-compose ${LOCAL_BIN}/docker-compose
+        cp "${CACHE}/docker-compose" ${LOCAL_BIN}/docker-compose
         chmod +x ${LOCAL_BIN}/docker-compose
     else
-        cp ${CACHE}/docker-compose ${BIN}/docker-compose
+        cp "${CACHE}/docker-compose" ${BIN}/docker-compose
         chmod +x ${BIN}/docker-compose
     fi
 }
@@ -247,15 +254,15 @@ __installDockerCompose()  {
 __installAwsCli () {
     __createCacheDir
 
-    if [[ ! -e ${CACHE}/aws ]] ; then
-        curl -Ls "https://raw.githubusercontent.com/mesosphere/aws-cli/master/aws.sh" -o ${CACHE}/aws
+    if [[ ! -e "${CACHE}/aws" ]] ; then
+        curl -Ls "https://raw.githubusercontent.com/mesosphere/aws-cli/master/aws.sh" -o "${CACHE}/aws"
     fi
 
     if [[ $runningLocally == true ]] ; then
-        cp ${CACHE}/aws ${LOCAL_BIN}/aws
+        cp "${CACHE}/aws" ${LOCAL_BIN}/aws
         chmod +x ${LOCAL_BIN}/aws
     else
-        cp ${CACHE}/aws ${BIN}/aws
+        cp "${CACHE}/aws" ${BIN}/aws
         chmod +x ${BIN}/aws
     fi
 }
@@ -264,36 +271,19 @@ __installAwsCli () {
 __installWeaveworks()  {
     __createCacheDir
 
-    if [[ ! -e ${CACHE}/scope ]] ; then
-        curl -L git.io/scope -o ${CACHE}/scope
+    if [[ ! -e "${CACHE}/scope" ]] ; then
+        curl -L git.io/scope -o "${CACHE}/scope"
     fi
 
     if [[ $runningLocally == true ]] ; then
-        cp ${CACHE}/scope ${LOCAL_BIN}/scope
+        cp "${CACHE}/scope" ${LOCAL_BIN}/scope
         chmod +x ${LOCAL_BIN}/scope
     else
-        cp ${CACHE}/scope ${BIN}/scope
+        cp "${CACHE}/scope" ${BIN}/scope
         chmod +x ${BIN}/scope
     fi
 }
 
-
-__setupWindowsLinks() {
-    if [[ "$WINDOWS_HOME" != "unknown" ]] ; then
-        if [[ -e ${WINDOWS_HOME}/Documents && ! -e ${VM_HOME}/Documents ]] ; then
-            ln -s ${WINDOWS_HOME}/Documents ${VM_HOME}/Documents
-        fi
-        if [[ -e ${WINDOWS_HOME}/Downloads && ! -e ${VM_HOME}/Downloads ]] ; then
-            ln -s ${WINDOWS_HOME}/Downloads ${VM_HOME}/Downloads
-        fi
-        if [[ -e ${WINDOWS_HOME}/repositories && ! -e ${VM_HOME}/Repositories ]] ; then
-            ln -s ${WINDOWS_HOME}/repositories ${VM_HOME}/Repositories
-        fi
-        if [[ -e ${WINDOWS_HOME}/repositories/task-data && ! -e ${VM_HOME}/task-data ]] ; then
-            ln -s ${WINDOWS_HOME}/repositories/task-data ${VM_HOME}/task-data
-        fi
-    fi
-}
 
 __checkUser
 __identifyWindowsUser
@@ -306,7 +296,7 @@ __step "Adding bin to path"
 __setupPath
 
 __step "Setting up custom prompt"
-__setupPrompt
+__setupProfileFiles
 
 __step "Installing bash"
 __installBash
@@ -319,9 +309,6 @@ __installAwsCli
 
 __step "Installing weaveworks scope"
 __installWeaveworks
-
-__step "Setting up links to common directories"
-__setupWindowsLinks
 
 echo
 echo "Now type: exec bash -l"
