289b3d50dd2960a758494a2a247cbaae9f07e5e1
Ian Finch <ianfinch@users.noreply.github.com>
2019-12-17 12:15:22 +0000

    Changed prompt script to use Powerline chevrons

--- resources/set-prompt.sh
+++ resources/set-prompt.sh
@@ -1,14 +1,33 @@
+# Set up prompt (tried with solarized)
+#
+# Notes
+#  - uses Powerline font for chevrons
+#  - the \[ ... \] construct stops contained characters being used in prompt length calculation
+
 export TERM=xterm-color
 
 if [[ `whoami` == "root" ]] ; then
-   prompt="#"
-   colour="31"
+   swatch=(124 160 202 235)
 else
-   prompt="$"
-   colour="36"
+   swatch=(61 33 37 235)
 fi
 
+chevron=$'\xee\x82\xb0'
+
+setColour() {
+    echo "\[\e[48;5;${swatch[$1]};38;5;230m\]"
+}
+
+chevron() {
+    chevronChar=$'\xee\x82\xb0'
+    echo "\[\e[48;5;${swatch[$2]};38;5;${swatch[$1]}m\]$chevronChar"
+}
+
+machine="$(setColour 0)\h$(chevron 0 1)"
+username="$(setColour 1)\u$(chevron 1 2)"
+directory="$(setColour 2)\w$(chevron 2 3)"
+
 setTitle='\e]0;\u@\h:\W\a'
-setPrompt="[\e[0;${colour}m\u\e[m@\e[0;35m\h\e[m:\e[0;32m\w\e[m]$prompt "
+setPrompt="${machine}${username}${directory}\[\e[m\] "
 
 export PS1="$setTitle$setPrompt"
