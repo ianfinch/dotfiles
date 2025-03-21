82f18e7e04a92667dab567dcd1928cfd6853cbac
Ian Finch <ianfinch@users.noreply.github.com>
2025-03-21 23:57:06 +0000

    Improved xcolors script

--- scripts/xcolors.sh
+++ scripts/xcolors.sh
@@ -11,4 +11,30 @@ if [[ ! -e "${xcolors}" ]] ; then
     exit 1
 fi
 
-cat ${xcolors} | jq -r '.[] | [.number, .name] | join(": ")'
+n=1
+cat ${xcolors} | jq -r '.[] | ( .number | tostring) + " " + .name + " " + .hex' | while read line ; do
+
+    code=$( echo ${line} | cut -d' ' -f1 )
+    name=$( echo ${line} | cut -d' ' -f2 )
+    hex=$( echo ${line} | cut -d' ' -f3 )
+    r=$( echo ${hex} | cut -c2-3 )
+    g=$( echo ${hex} | cut -c4-5 )
+    b=$( echo ${hex} | cut -c6-7 )
+    brightness=$( expr $((16#${r})) + $((16#${g})) + $((16#${b})) )
+
+    #echo ${brightness}
+    if [[ ${brightness} -gt 380 ]] ; then
+
+        printf "\e[48:5:${code};30m %3d \e[m" ${code}
+    else
+
+        printf "\e[48:5:${code};97m %3d \e[m" ${code}
+    fi
+
+    n=$( expr ${n} + 1)
+    if [[ $n -eq 17 ]] ; then
+
+        n=1
+        echo
+    fi
+done
